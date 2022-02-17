e48d24aea2fd64b03fc97bc02d790636a8c2fb25
tungnt <tungnt@ominext.com>
2022-02-17 17:32:31 +0700

    build login

--- config/routes.php
+++ config/routes.php
@@ -45,18 +45,15 @@ return static function (RouteBuilder $routes) {
     $routes->setRouteClass(DashedRoute::class);
 
     $routes->scope('/', function (RouteBuilder $builder) {
-        /*
-         * Here, we are connecting '/' (base path) to a controller called 'Pages',
-         * its action called 'display', and we pass a param to select the view file
-         * to use (in this case, templates/Pages/home.php)...
-         */
+ 
         $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
-        /*
-         * ...and connect the rest of 'Pages' controller's URLs.
-         */
         $builder->connect('/pages/*', 'Pages::display');
-
+        $builder->connect(
+            '/login',
+            ['controller'=>'Pages', 'action'=>'login'],
+            ['_name'=>'login']
+        );
         /*
          * Connect catchall routes for all controllers.
          *
---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'email' => 'Lorem ipsum dolor sit amet',
+                'email_verified' => 1,
+                'password' => 'Lorem ipsum dolor sit amet',
+                'remember_token' => 'Lorem ipsum dolor sit amet',
+                'is_admin' => 1,
+                'role' => 'Lorem ip',
+                'active' => 1,
+                'status' => 1,
+                'create_at' => '2022-02-17 09:38:31',
+                'modified' => '2022-02-17 09:38:31',
+            ],
+        ];
+        parent::init();
+    }
+}
