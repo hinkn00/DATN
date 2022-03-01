6410ca9aaaa1bf7758c89936699c4ca93e1c688b
tungnt <tungnt@ominext.com>
2022-03-01 16:55:28 +0700

    admin_users-curd

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -52,16 +52,16 @@ class UsersController extends AdminController
     public function index()
     {
         $param_limit = $this->request->getParam('limit');
-        $this->loadModel('Users');
-        $users = $this->paginate($this->Users->find('all'),array('limit'=> isset($param_limit)? $param_limit : '10'));
+        $this->loadModel('UsersBase');
+        $users = $this->paginate($this->UsersBase->find('all'),array('limit'=> isset($param_limit)? $param_limit : '10'));
         $this->set('users',$users);
         $this->set('title','Quản lý users');
     }
     public function add()
     {
-        $this->loadModel('Users');
+        $this->loadModel('UsersBase');
         
-        $user = $this->Users->newEmptyEntity();
+        $user = $this->UsersBase->newEmptyEntity();
         if($this->request->is('post')){
             // $userTable = TableRegistry::get('Users');
             $user->name = $this->request->getData('name');
@@ -87,7 +87,59 @@ class UsersController extends AdminController
     }
     public function edit($id = null)
     {
+        $id = $this->request->getParam('id');
+        $this->loadModel('UsersBase');
+        $user = $this->UsersBase->get($id);
+        $this->set('user', $user);
+        $data = [];
         
+        if($this->request->is(array('post','put'))){
+            $data['name'] = $this->request->getData('name');
+            $data['email'] = $this->request->getData('email');
+            $data['role'] = $this->request->getData('role');
+            $data['img_avatar'] = $this->request->getData('img_avatar');
+            $user = $this->UsersBase->patchEntity($user, [
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'role' => $data['role'],
+            ]);
+            
+            $img = $data['img_avatar'];
+            if($img->getError() == 0){
+                //check file upload
+                if($user->img_avatar){
+                    unlink(WWW_ROOT.'img/upload/users/'.$user->img_avatar);
+                }
+                $user->img_avatar = $this->uploadFile($img);
+            }
+            
+            //save db
+            if($this->UsersBase->save($user)){
+                $this->Flash->success(__('success'));
+                return $this->redirect(['action'=>'edit', 'id'=> $id]);
+            }else{
+                $this->set('error','Cập nhật không thành công! Vui lòng thử lại');
+            }
+        }
+        
+        $this->set('title','Chỉnh sửa thông tin của '.$user->name);
+    }
+
+    public function delete($id = null)
+    {
+        $id = $this->request->getParam('id');
+        $this->request->allowMethod(['post','delete']);
+        $this->loadModel('UsersBase');
+        $user = $this->UsersBase->get($id);
+        if($user->img_avatar){
+            unlink(WWW_ROOT.'img/upload/users/'.$user->img_avatar);
+        }
+        if($this->UsersBase->delete($user)){
+            $this->Flash->success(__('success'));
+            return $this->redirect(['action'=>'index']);
+        } else{
+            $this->set('error','Xóa không thành công! Vui lòng thử lại');
+        }
     }
     //upload avatar
     public function uploadFile($img)
